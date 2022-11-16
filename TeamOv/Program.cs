87771e6fc89c14498e538f3a8c99eed2c6e8b192
ullsten <113597614+ullsten@users.noreply.github.com>
2022-11-16 12:28:38 +0100

    Add project files.

---
+++ TeamOv/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamOv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Team OV Refresh start");
+        }
+    }
+}
