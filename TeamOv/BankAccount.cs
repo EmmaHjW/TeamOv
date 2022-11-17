c40a616db2d093143b49e96ea186ad3f37488400
ewahlstrom90@gmail.com <ewahlstrom90@gmail.com>
2022-11-17 09:22:50 +0100

    Added class and some props, methods and a ToString in BankAccount.

--- TeamOv/BankAccount.cs
+++ TeamOv/BankAccount.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace TeamOv
 {
-    internal class BankAccount
+    public class BankAccount
     {
+        public string AccountNumber { get; init; }
+        public string AccountType { get; set; }
+        public decimal Money { get; set; }
+        public bool Active { get; set; }
+        public decimal InterestRate { get; set; }
+        public string AccountName { get; set; }
+        public Currency Currency { get; set; }
+        public Customer Owner { get; set; } // TODO should it be possible to change owner to correct an error or to switch other owner?
+
+        public BankAccount(
+            string accountNumber,
+            decimal interestRate,
+            string accountName,
+            Customer owner,
+            bool active = false,
+            decimal money = 0m,
+            Currency currency = Currency.SEK,
+            string accountType = ""
+        )
+        {
+            AccountNumber = accountNumber;
+            AccountType = accountType;
+            Money = money;
+            InterestRate = interestRate;
+            AccountName = accountName;
+            Active = active;
+            Currency = currency;
+            Owner = owner;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Money += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Money -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(AccountNumber)}: {AccountNumber}, {nameof(AccountType)}: {AccountType}, {nameof(Money)}: {Money}, {nameof(Active)}: {Active}, {nameof(InterestRate)}: {InterestRate}, {nameof(AccountName)}: {AccountName}, {nameof(Currency)}: {Currency}, {nameof(Owner.NumericId)}: {Owner.NumericId}";
+        }
     }
 }
+
