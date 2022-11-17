30eb68043c736d37cf778584d43bf8203a296071
ewahlstrom90@gmail.com <ewahlstrom90@gmail.com>
2022-11-17 08:58:47 +0100

    Added classes

---
+++ TeamOv/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamOv
+{
+    public class Customer : User
+    {
+        public Customer(string? userName, string? password, int userId, bool active, bool IsAdmin) : base(userName, password, userId, active, IsAdmin)
+        {
+        }
+    }
+}
