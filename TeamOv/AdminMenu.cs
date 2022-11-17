d9eea99a5bede585c28ac62046e1ee5f8b4a2d30
ewahlstrom90@gmail.com <ewahlstrom90@gmail.com>
2022-11-17 09:44:34 +0100

    Changed methods to an other class, AdminMenu

---
+++ TeamOv/AdminMenu.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+using AnsiConsole = Spectre.Console.AnsiConsole;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace TeamOv
+{
+    public static class AdminMenu
+    {
+        public static void ShowAdminScreen()
+        {
+            while (true)
+            {
+                Console.Clear();
+                var grid = new Grid();
+
+                // Add columns 
+                grid.AddColumn();
+                grid.AddColumn();
+                grid.AddColumn();
+                grid.AddColumn();
+                grid.AddColumn();
+                Console.WriteLine("                         Welcome to VaennikATM");
+
+                // Add header row 
+                grid.AddRow(new Text[]{
+                new Text(" ").LeftAligned(),
+                new Text(" ").Centered(),
+                new Text(" ").Centered(),
+                new Text(" ").RightAligned(),
+                new Text(" ").RightAligned()
+                });
+
+                // Add content row 
+                grid.AddRow(new Text[]{
+                new Text("(P)rint customers", new Style(Color.Green, Color.Black)).Centered(),
+                new Text("(C)reate customer", new Style(Color.Green, Color.Black)).Centered(),
+                new Text("(D)elete customer", new Style(Color.Green, Color.Black)).Centered(),
+                new Text("(L)ogout", new Style(Color.Green, Color.Black)).Centered(),
+                });
+
+                AnsiConsole.Write(grid);
+
+                string customerOptions = Console.ReadLine();
+
+                switch (customerOptions.ToLower())
+                {
+                    case "p":
+                        UserService.PrintAllCustomers();
+                        Console.ReadLine();
+                        break;
+                    case "c":
+                        CreateCustomerScreen();
+                        Console.ReadLine();
+                        break;
+                    case "d":
+                        Console.WriteLine("Delete customer");
+                        Console.ReadLine();
+                        break;
+                    case "l":
+                        Console.WriteLine("You going to be logged out..");
+                        Console.WriteLine("_");
+                        Thread.Sleep(300);
+                        Console.WriteLine("_");
+                        Thread.Sleep(300);
+                        Console.WriteLine("_");
+                        Thread.Sleep(300);
+                        Console.WriteLine("logged out complete");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        continue;
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static void CreateCustomerScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("Input customer data");
+            bool completed = false;
+            do
+            {
+                Console.Write("Input username: ");
+                var username = Console.ReadLine();
+                if (UserService.UserExists(username))
+                {
+                    Console.WriteLine("Duplicate username!");
+                    continue;
+                }
+                Console.Write("Input password: ");
+                var password = Console.ReadLine();
+                bool? active;
+                do
+                {
+                    Console.WriteLine("active or inactive");
+                    active = Console.ReadLine() switch
+                    {
+                        "active" => true,
+                        "inactive" => false,
+                        _ => null
+                    };
+                } while (active is null);
+
+                completed = UserService.AddUser(username, password, (bool)active);
+                if (!completed)
+                {
+                    Console.WriteLine("Operation failed. No user added.");
+                }
+            } while (completed == false);
+        }
+    }
+}
